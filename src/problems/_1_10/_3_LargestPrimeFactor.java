29fc3267f20b429a17104fa7223949a388bed760
Isaac Yocum <isyocum@gmail.com>
2021-09-07 19:31:56 -0600

    Completed problem 3

--- src/problems/_1_10/_3_LargestPrimeFactor.java
+++ src/problems/_1_10/_3_LargestPrimeFactor.java
@@ -1,13 +1,46 @@
 package problems._1_10;
 
+import java.util.ArrayList;
+
 public class _3_LargestPrimeFactor {
     public static void main(String[] args) {
-        largestPrimeFactor();
+        long target = 600851475143L;
+        ArrayList<Integer> primeNumbersUntil100 = getPrimeNumbersUntil(10000);
+
+        System.out.println(primeNumbersUntil100);
+
+        ArrayList<Integer> numsTargetIsDivisibleBy = new ArrayList<>();
+
+        for (int i = primeNumbersUntil100.size() - 1; i > 0; i--) {
+            if (target % primeNumbersUntil100.get(i) == 0) {
+                numsTargetIsDivisibleBy.add(primeNumbersUntil100.get(i));
+                target /= primeNumbersUntil100.get(i);
+            }
+        }
+
+        System.out.println("Prime factors = " + numsTargetIsDivisibleBy);
+        System.out.println(target);
     }
 
-    public static void largestPrimeFactor() {
-        long target = 600851475143L;
+    public static ArrayList<Integer> getPrimeNumbersUntil(int limit) {
 
+        ArrayList<Integer> listOfPrimeFactors = new ArrayList<>();
 
+        for (int i = 1; i < limit; i++) {
+            boolean primeCandidate = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primeCandidate = false;
+                    break;
+                }
+            }
+
+            if (primeCandidate) {
+                listOfPrimeFactors.add(i);
+            }
+        }
+
+        return listOfPrimeFactors;
     }
 }
