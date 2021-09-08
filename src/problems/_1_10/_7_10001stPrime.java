c995741948a2b18e1611c088c4dee374218d2b03
Isaac Yocum <isyocum@gmail.com>
2021-09-07 20:29:31 -0600

    added problem 8

--- src/problems/_1_10/_7_10001stPrime.java
+++ src/problems/_1_10/_7_10001stPrime.java
@@ -4,7 +4,8 @@ import utils.PrimeNumbersUtil;
 
 public class _7_10001stPrime {
     public static void main(String[] args) {
-        int primeNum = PrimeNumbersUtil.getNthPrimeNumber(10001);
-        System.out.println("The 10001st prime number is: " + primeNum);
+        int target = 10001;
+        int primeNum = PrimeNumbersUtil.getNthPrimeNumber(target);
+        System.out.println("The " + target + " prime number is: " + primeNum);
     }
 }
---
+++ src/problems/_1_10/_8_LargestProductInASeries.java
@@ -0,0 +1,4 @@
+package problems._1_10;
+
+public class _8_LargestProductInASeries {
+}
